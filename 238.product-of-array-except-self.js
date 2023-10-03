33356d724541de34b5b3d6a30be2cbefa7c8d5f0
igor-mircic <igor.mircic@live.com>
2023-10-04 00:16:42 +0200

    solved 238

--- 238.product-of-array-except-self.js
+++ 238.product-of-array-except-self.js
@@ -10,7 +10,19 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  return nums;
+  let result = Array(nums.length).fill(1);
+  let prefix = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+  result;
+  let postfix = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= postfix;
+    postfix *= nums[i];
+  }
+  return result;
 };
 // @lc code=end
 export default productExceptSelf;
--- 238.product-of-array-except-self.test.js
+++ 238.product-of-array-except-self.test.js
@@ -3,6 +3,11 @@ import productExceptSelf from "./238.product-of-array-except-self";
 
 describe("#238.product-of-array-except-self", () => {
   it("Case 1", () => {
-    expect(productExceptSelf([1, 2, 3, 4])).toBe([24, 12, 8, 6]);
+    expect(productExceptSelf([1, 2, 3, 4])).toStrictEqual([24, 12, 8, 6]);
+  });
+  it("Case 2", () => {
+    expect(productExceptSelf([-1, 1, 0, -3, 3])).toStrictEqual([
+      -0, 0, 9, -0, 0,
+    ]);
   });
 });
